897148478fb0ffb948608752aa4d465507761bad
bulan506 <116318596+bulan506@users.noreply.github.com>
2022-11-04 22:50:47 -0600

    sigue igual, sin cambios
    no cambios

--- ProyectoGato 2.0/src/proyectogato/Main.java
+++ ProyectoGato 2.0/src/proyectogato/Main.java
@@ -1,30 +1,19 @@
 package proyectogato;
 
-
 import javafx.application.Application;
-
-//import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
 /**
  *
- * @author Usuario
+ * @author Brandon Vargas
  */
-
 public class Main extends Application {
-    
     @Override
     public void start(Stage primaryStage) {
         Interface iE = new Interface();
       //  Scene gameScene = new Scene(iE.welcomeScene(),550,550);
-        
         iE.logeo();
     }
-
-  
     public static void main(String[] args) {
         launch(args);
-        
     }
-    
 }
